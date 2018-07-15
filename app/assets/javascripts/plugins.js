f59cac5e54dd4affe2d40c4453f0f6a48873f7ef
Lena <fatmeow@gmail.com>
2018-07-15 18:00:52 -0400

    feat: Add Sign up and Login pages

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -94,14 +94,30 @@ $(window).scroll(function(){
 			var difference = $(window).scrollTop() - $(this).offset().top;
 			// Top value of image is set to half the amount scrolled
 			// (this gives the illusion of the image scrolling slower than the rest of the page)
-			var half = (difference / 2) - 200 + 'px';
+			var half = (difference / 2) + 'px';
 
 			$(this).find('img').css('top', half);
 		} else {
 			// if image is below the top of the window set top to 0
-			$(this).find('img').css('top', '-200px');
+			$(this).find('img').css('top', '0px');
 		}
 	});
+  // Add parallax scrolling to all images in .paralax-image container
+  $('#home-parallax-image').each(function(){
+    // only put top value if the window scroll has gone beyond the top of the image
+    if ($(this).offset().top < $(window).scrollTop()) {
+      // Get ammount of pixels the image is above the top of the window
+      var difference = $(window).scrollTop() - $(this).offset().top;
+      // Top value of image is set to half the amount scrolled
+      // (this gives the illusion of the image scrolling slower than the rest of the page)
+      var half = (difference / 2) - 200 + 'px';
+
+      $(this).find('img').css('top', half);
+    } else {
+      // if image is below the top of the window set top to 0
+      $(this).find('img').css('top', '-200px');
+    }
+  });
 });
 
 $(function () {
