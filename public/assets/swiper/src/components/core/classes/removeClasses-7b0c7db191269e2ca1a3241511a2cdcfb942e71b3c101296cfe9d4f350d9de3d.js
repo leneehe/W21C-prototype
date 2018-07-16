acff922b5c01df0c3bf504880a5b8c8a541c3d01
Situ William <situ_william@yahoo.com>
2018-07-15 22:18:41 -0400

    preparing for deployment

---
+++ public/assets/swiper/src/components/core/classes/removeClasses-7b0c7db191269e2ca1a3241511a2cdcfb942e71b3c101296cfe9d4f350d9de3d.js
@@ -0,0 +1,7 @@
+export default function () {
+  const swiper = this;
+  const { $el, classNames } = swiper;
+
+  $el.removeClass(classNames.join(' '));
+}
+;
---
+++ public/assets/swiper/src/components/core/translate/maxTranslate-76d3b723e5d600b8f9d8d1546ac16bd127a2218a8193ec0a82017f82e984fa5d.js
@@ -0,0 +1,4 @@
+export default function () {
+  return (-this.snapGrid[this.snapGrid.length - 1]);
+}
+;
