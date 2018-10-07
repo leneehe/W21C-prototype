f9cdcfe30bc624c71a64e05593d39d51ad0f4059
Situ William <situ_william@yahoo.com>
2018-10-07 12:43:21 -0400

    chore: change sqlite3 to pg for database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_07_030541) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "checklists", force: :cascade do |t|
     t.string "description"
     t.integer "event_id"
