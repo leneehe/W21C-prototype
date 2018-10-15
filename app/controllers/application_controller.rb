d96b96060ee7e6a9e019eb4b43d67ded5e593770
Situ William <situ_william@yahoo.com>
2018-10-14 21:52:51 -0400

    feat: add devise redirect to dashboard after sign-in sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    dashboard_index_path
+  end
+  def after_sign_up_path_for(resource)
+    dashboard_index_path
+  end
 end
