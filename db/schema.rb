c4cf8129b39372fe4dbd28cc4d7566f798cadd6d
Lena <fatmeow@gmail.com>
2018-10-14 22:08:10 -0400

    feat: add admin and date of birth field to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_185222) do
+ActiveRecord::Schema.define(version: 2018_10_15_020110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,7 +119,8 @@ ActiveRecord::Schema.define(version: 2018_10_07_185222) do
     t.string "first_name"
     t.string "last_name"
     t.string "gender"
-    t.string "role"
+    t.boolean "admin", default: false
+    t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
