2fd0432781862a2e3c394ad4a134525ad8ac8d2d
Situ William <situ_william@yahoo.com>
2018-11-26 21:32:53 -0500

    feat: Add default name for valuetypes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_023753) do
+ActiveRecord::Schema.define(version: 2018_11_27_023022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,7 +155,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_023753) do
   end
 
   create_table "value_types", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "Amount"
     t.bigint "health_condition_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
