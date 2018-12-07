22bb183ac5790a8d53222fc4955ba0c7d9052c5d
Lena <fatmeow@gmail.com>
2018-12-07 02:11:22 -0500

    feat: save complete/imcomplete entries

--- app/controllers/dashboard/plan/checklists_controller.rb
+++ app/controllers/dashboard/plan/checklists_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::Plan::ChecklistsController < ApplicationController
   def index
   end
-  
+
   def create
     event = Event.find(params[:event_id])
     @entry = Checklist.new(checklist_params)
@@ -18,6 +18,12 @@ class Dashboard::Plan::ChecklistsController < ApplicationController
   end
 
   def update
+    @entry = Checklist.find(params[:id])
+    @entry.complete ? @entry.complete = false : @entry.complete = true
+    @entry.save
+    respond_to do |format|
+      format.html { redirect_to dashboard_plan_event_url(params[:event_id]), notice: 'Entry was successfully updated.' }
+    end
   end
 
   def destroy
