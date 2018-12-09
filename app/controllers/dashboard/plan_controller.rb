d5e68ccbceee63208b46b155dda4f06b4aea303d
Lena <fatmeow@gmail.com>
2018-12-09 16:34:31 -0500

    fix: journals link on routes

--- app/controllers/dashboard/plan_controller.rb
+++ app/controllers/dashboard/plan_controller.rb
@@ -2,6 +2,8 @@ class Dashboard::PlanController < ApplicationController
   layout 'main/layout-2'
 
   def index
+    @goals = current_user.goals
+    @events = current_user.events.order(start: :desc).limit(5)
   end
 
   def checklists
