4d8dd38f6a7893aeb2df5e4bb4a2336bcf642fba
Situ William <situ_william@yahoo.com>
2018-12-10 21:34:11 -0500

    feat: Added charts to display_report method

--- app/controllers/dashboard/plan_controller.rb
+++ app/controllers/dashboard/plan_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::PlanController < ApplicationController
+  helper_method :build_data_collection
   layout 'main/layout-2'
 
   def index
@@ -15,6 +16,8 @@ class Dashboard::PlanController < ApplicationController
   end
 
   def display_report
+    @user_conditions = current_user.health_conditions
+
     respond_to do |format|
       format.html
       format.pdf do
@@ -25,6 +28,21 @@ class Dashboard::PlanController < ApplicationController
         layout: "layouts/pdf.html.erb"
       end
     end
+  end
+
+  def build_data_collection(condition, unit)
+    @value_collection = Array.new
+    @value_data = Array.new
+
+     condition.tracked_health_conditions.one_week_ago.group_by(&:value_type_id).each do |key, value|
+      collection_name =  ValueType.find(key).name
+      value.each do |measurement|
+        @value_data.push({"x" => measurement.created_at, "y" => measurement.severity_score})  
+      end  
+      @value_collection.push({ "name" => collection_name, "meta" => collection_name, "data" => @value_data, "unit_of_measure" => unit })
+      @value_data = []
+    end
+    return @value_collection
   end
 
 end
